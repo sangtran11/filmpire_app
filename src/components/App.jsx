2a6c058915d2b63947e13f96fb94423c83b41ad8
SangTran <sang.tran@adurolife.com>
2022-06-10 16:11:18 +0700

    Build files structure, set up routing and styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Movies, Actors, MovieInformation, NavBar, Profile } from "./index";
 
+import useStyles from "./styles";
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>App</div>
-  )
-}
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />}></Route>
+          <Route exact path="/movie/:id" element={<MovieInformation />}></Route>
+          <Route exact path="/actors/:id" element={<Actors />}></Route>
+          <Route exact path="/profile/:id" element={<Profile />}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
